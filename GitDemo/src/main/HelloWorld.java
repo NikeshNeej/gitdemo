7caff04201ea31645ebb4c223e0c2e0166d6c8be
Hp <Hp@DESKTOP-39IL56N>
2021-03-22 21:11:26 +0530

    Mul Added

--- GitDemo/src/main/HelloWorld.java
+++ GitDemo/src/main/HelloWorld.java
@@ -12,6 +12,9 @@ public class HelloWorld {
 	 public static void div(int num1,int num2){
 	    System.out.println(num1/num2);
 	  }
+	 public static void mil(int num1,int num2){
+		    System.out.println(num1*num2);
+		  }
 
 }
 
