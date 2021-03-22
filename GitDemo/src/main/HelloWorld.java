cdea0fa5a2094f9b3fe68e46909c083989f865cc
Hp <Hp@DESKTOP-39IL56N>
2021-03-22 21:02:24 +0530

    HelloWorld

---
+++ GitDemo/src/main/HelloWorld.java
@@ -0,0 +1,14 @@
+package main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+	}
+	  public static void add(int num1,int num2){
+	    System.out.println(num1+num2);
+	  }
+
+}
+
