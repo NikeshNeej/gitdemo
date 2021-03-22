73d77ba330b6021e7f7b3a79635752716c078c8b
NikeshNeej <81173983+NikeshNeej@users.noreply.github.com>
2021-03-22 21:09:46 +0530

    Update HelloWorld.java
    Div Method Added.

--- GitDemo/src/main/HelloWorld.java
+++ GitDemo/src/main/HelloWorld.java
@@ -9,6 +9,9 @@ public class HelloWorld {
 	  public static void sub(int num1,int num2){
 	    System.out.println(num1-num2);
 	  }
+	 public static void div(int num1,int num2){
+	    System.out.println(num1/num2);
+	  }
 
 }
 
